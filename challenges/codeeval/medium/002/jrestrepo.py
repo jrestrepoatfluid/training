4f6086abe1262d2851419eaaeeb251baf62e6d12
jrestrepoatfluid <jrestrepo@fluid.la>
2017-06-05 13:18:35 -0500

    Create jrestrepo.py

---
+++ challenges/codeeval/medium/002/jrestrepo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Codeval challenge17"""
+
+
+def get_tests():
+    """Get data"""
+    casos = []
+    with open("sys.arg[0]", "r") as insumos:
+        for indice in insumos:
+            casos.append(indice.strip())
+    return casos
+
+def main():
+    """main function"""
+    tests = get_tests()
+    data = {}
+    phrase = int(tests[0])
+    for test in tests:
+        data[len(test)] = test
+    for i in range(phrase):
+        print(data[max(data)])
+        del data[max(data)]
+
+
+main()
